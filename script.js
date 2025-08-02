90572a01750db75953d80f126c6a3fed62457f49
BaraDroid <lambeinovab@seznam.cz>
2025-08-02 20:46:07 +0200

    feat: show correct and incorrect answers, if clicked

--- script.js
+++ script.js
@@ -1,68 +1,69 @@
 let questionsCounter = 0; //ten se bude navysovat pri kazdym Durchlaufu
 
-        // <div class="quiz_tab" id="quizTab">
-        //     <h1>Quiz</h1>
-        //     <h2 id="question">Wie viele Augen hat ein Mensch?</h2>
-        //     <div class="answer_container">
-        //         <button class="one_answer incorrect" id="answerIndex0">Möglichst viele.</button>
-        //         <button class="one_answer">Nur ein einziges in der Stirnmitte.</button>
-        //         <button class="one_answer">Zwei.</button>
-        //         <button class="one_answer correct">Es ist eine Trickfrage, ein Mensch hat keine Augen.</button>
-        //     </div>
-        //     <div class="button_bar">
-        //         <button class="action_button">Lösung</button>
-        //         <button class="action_button">Weiter</button>
-        //     </div>
-        // </div>
+// <div class="quiz_tab" id="questionId">
+//     <h1>Quiz</h1>
+//     <h2 id="question">Wie viele Augen hat ein Mensch?</h2>
+//     <div class="answer_container">
+//         <button class="one_answer incorrect" id="answerIndex0">Möglichst viele.</button>
+//         <button class="one_answer">Nur ein einziges in der Stirnmitte.</button>
+//         <button class="one_answer">Zwei.</button>
+//         <button class="one_answer correct">Es ist eine Trickfrage, ein Mensch hat keine Augen.</button>
+//     </div>
+//     <div class="button_bar">
+//         <button class="action_button">Lösung</button>
+//         <button class="action_button">Weiter</button>
+//     </div>
+// </div>
 
 function renderQuestion() {
-    if (questionsCounter <= quizQuestions.length - 1) {
-        getQuizCardTemplate();
-    }
-    
+  let questionIndex = questionsCounter;
+  if (questionsCounter <= quizQuestions.length - 1) {
+    getQuizCardTemplate(questionIndex);
+    questionsCounter++;
+  } else {
+    renderFinalTab();
+    //statistiky
+    //dat pocitadlo na nulu
+  }
 }
 
 document.addEventListener("DOMContentLoaded", renderQuestion);
 
-function getQuizCardTemplate() {
-  let questionIndex = questionsCounter;
-
+function getQuizCardTemplate(index) {
   let wrapper = document.getElementById("wrapper");
-  wrapper.innerHTML = '';
-  let cardTab = document.createElement('div');
-  cardTab.className = 'quiz_tab';
-  let title = document.createElement('h1');
-  title.textContent = 'Quiz';
-
+  wrapper.innerHTML = "";
+  let cardTab = document.createElement("div");
+  cardTab.className = "quiz_tab";
+  cardTab.id = quizQuestions[index].questionId;
+  let title = document.createElement("h1");
+  title.textContent = "Quiz";
 
   let questionTitle = document.createElement("h2");
   questionTitle.id = "question";
-  questionTitle.innerHTML = quizQuestions[questionIndex].questionName;
+  questionTitle.innerHTML = quizQuestions[index].questionName;
 
   let answers = document.createElement("div");
   answers.classList.add("answer_container");
 
-  let answerA = document.createElement("button");
-  answerA.classList.add("one_answer");
-  answerA.innerHTML = quizQuestions[questionIndex].answers[0].answerContent;
-
-  let answerB = document.createElement("button");
-  answerB.classList.add("one_answer");
-  answerB.innerHTML = quizQuestions[questionIndex].answers[1].answerContent;
-
-  let answerC = document.createElement("button");
-  answerC.classList.add("one_answer");
-  answerC.innerHTML = quizQuestions[questionIndex].answers[2].answerContent;
-
-  let answerD = document.createElement("button");
-  answerD.classList.add("one_answer");
-  answerD.innerHTML = quizQuestions[questionIndex].answers[1].answerContent;
+  quizQuestions[index].answers.forEach((oneAnswer) => {
+    let answerBtn = document.createElement("button");
+    answerBtn.classList.add("one_answer");
+    answerBtn.innerHTML = oneAnswer.answerContent;
+    answerBtn.id = oneAnswer.answerId;
+    answerBtn.setAttribute(
+      "onclick",
+      `validateAnswer(${cardTab.id}, '${oneAnswer.answerId}')`
+    );
+    answers.appendChild(answerBtn);
+  });
 
   let btnBar = document.createElement("div");
   btnBar.classList.add("button_bar");
   let solveBtn = document.createElement("button");
   solveBtn.className = "action_button";
   solveBtn.textContent = "Lösen";
+  solveBtn.setAttribute("onclick",`showSolution(${cardTab.id})`);
+
   let nextBtn = document.createElement("button");
   nextBtn.className = "action_button";
   nextBtn.textContent = "Weiter";
@@ -72,14 +73,29 @@ function getQuizCardTemplate() {
   cardTab.appendChild(title);
   cardTab.appendChild(questionTitle);
   cardTab.appendChild(answers);
-  answers.appendChild(answerA);
-  answers.appendChild(answerB);
-  answers.appendChild(answerC);
-  answers.appendChild(answerD);
   cardTab.appendChild(btnBar);
   btnBar.appendChild(solveBtn);
   btnBar.appendChild(nextBtn);
+}
 
-  questionsCounter++;
-  console.log(questionsCounter);
+function validateAnswer(questionId, replyId) {
+  let currentQuestion = quizQuestions[questionId - 1];
+  console.log(replyId);
+  let correctAnswer = currentQuestion.answers.find((answer) => {
+    return answer.correct;
+  });
+  if (correctAnswer.answerId === replyId) {
+    document.getElementById(replyId).classList.add("correct");
+  } else {
+    document.getElementById(replyId).classList.add("incorrect");
+    document.getElementById(correctAnswer.answerId).classList.add("correct");
+  }
+}
+
+function showSolution(questionId) {
+    let currentQuestion = quizQuestions[questionId - 1];
+    let correctAnswer = currentQuestion.answers.find((answer) => {
+    return answer.correct;
+  });
+  document.getElementById(correctAnswer.answerId).classList.add("correct");
 }
