a25b45b2852139c9f50bb6d40088e9a0f14f01ad
BaraDroid <lambeinovab@seznam.cz>
2025-08-07 22:55:56 +0200

    ref: app rework in Vite for better use of npm confetti

---
+++ quizApp2.0/src/main.js
@@ -0,0 +1,270 @@
+import './style.css'
+import { quizQuestions } from './questions.js'
+import JSConfetti from 'js-confetti';
+const jsConfetti = new JSConfetti();
+
+let questionsCounter = 0;
+let timerRunning = false;
+let quizRunning = false;
+let responseTimes = [];
+let timeTrackingIntervalId;
+let badAnswerCounter = 0;
+let goodAnswerCounter = 0;
+
+//*region One card as HTML template for renderQuestion function.
+// <div class="quiz_tab" id="questionId">
+//     <div class="title_container">
+//       <h1>Quiz</h1>
+//     </div>
+//     <h2 id="question">Wie viele Augen hat ein Mensch?</h2>
+//     <div class="answer_container">
+//         <button class="one_answer incorrect" id="answerIndex0">Möglichst viele.</button>
+//         <button class="one_answer">Nur ein einziges in der Stirnmitte.</button>
+//         <button class="one_answer">Zwei.</button>
+//         <button class="one_answer correct">Es ist eine Trickfrage, ein Mensch hat keine Augen.</button>
+//     </div>
+//     <div class="button_bar">
+//         <button class="action_button">Lösung</button>
+//         <button class="action_button">Weiter</button>
+//     </div>
+// </div>
+
+document.addEventListener("DOMContentLoaded", renderQuestion);
+//document.addEventListener('DOMContentLoaded', renderFinalTab);
+
+function renderQuestion() {
+  const questionIndex = questionsCounter;
+  if (questionsCounter <= quizQuestions.length - 1) {
+    quizRunning = true;
+    getQuizCardTemplate(questionIndex);
+    startTimeTracking();
+    questionsCounter++;
+  } else {
+    quizRunning = false;
+    renderFinalTab();
+    setCountersBack();
+  }
+}
+
+function getQuizCardTemplate(index) {
+  const wrapper = document.getElementById("wrapper");
+  wrapper.innerHTML = "";
+  const cardTab = document.createElement("div");
+  cardTab.className = "quiz_tab";
+  cardTab.id = quizQuestions[index].questionId;
+  const titleContainer = document.createElement("div");
+  titleContainer.classList.add("title_container");
+  const title = document.createElement("h1");
+  title.textContent = "Quiz";
+  const timerDiv = document.createElement("div");
+  timerDiv.id = 'timer';
+  const timerSpan = document.createElement("span");
+  timerSpan.id = 'timerText';
+  const timerImage = document.createElement("img");
+  timerImage.setAttribute("src", "/timer_icone.png");
+  timerImage.classList.add("timer_image");
+  const questionTitle = document.createElement("h2");
+  questionTitle.id = "question";
+  questionTitle.innerHTML = quizQuestions[index].questionName;
+
+  const answers = document.createElement("div");
+  answers.classList.add("answer_container");
+  const answerToShuffle = shuffleAnswers(quizQuestions[index].answers);
+  createAnswerButtons(answerToShuffle, cardTab.id, answers);
+
+  const btnBar = document.createElement("div");
+  btnBar.classList.add("button_bar");
+  const solveBtn = document.createElement("button");
+  solveBtn.className = "action_button";
+  solveBtn.textContent = "Lösen";
+  //solveBtn.setAttribute("onclick",`showSolution(${cardTab.id})`);
+  //Lösung über setAttribute funktioniert nicht in Vite
+  //oder moderne Lösung für unobtrusives JS:
+  solveBtn.addEventListener("click", () => {showSolution(cardTab.id)});
+
+  const nextBtn = document.createElement("button");
+  nextBtn.className = "action_button";
+  nextBtn.textContent = "Weiter";
+  nextBtn.addEventListener('click', () => {renderQuestion()});
+
+  wrapper.appendChild(cardTab);
+  cardTab.appendChild(titleContainer);
+  titleContainer.appendChild(title);
+  titleContainer.appendChild(timerDiv);
+  timerDiv.appendChild(timerImage);
+  timerDiv.appendChild(timerSpan);
+  cardTab.appendChild(questionTitle);
+  cardTab.appendChild(answers);
+  cardTab.appendChild(btnBar);
+  btnBar.appendChild(solveBtn);
+  btnBar.appendChild(nextBtn);
+}
+
+function validateAnswer(questionId, replyId) {
+  const currentQuestion = quizQuestions[questionId - 1];
+  const correctAnswer = currentQuestion.answers.find((answer) => {
+  return answer.correct;
+  });
+  if (correctAnswer.answerId === replyId) {
+    getGoodAnswerStyle(replyId);
+    stopTimeTracker();
+  } else {
+    getBadAnswerStyle(replyId);
+  }
+}
+
+function showSolution(questionId) {
+    stopTimeTracker();
+    const currentQuestion = quizQuestions[questionId - 1];
+    const correctAnswer = currentQuestion.answers.find((answer) => {
+    return answer.correct;
+  });
+  document.getElementById(correctAnswer.answerId).classList.add("correct");
+}
+
+function shuffleAnswers(array) {
+  let shuffledAnswers = [];
+  let usedIndexes = [];
+  let i = 0;
+  while(i < array.length) {
+    let randomIndex = Math.floor(Math.random() * array.length);
+    if(!usedIndexes.includes(randomIndex)) {
+      shuffledAnswers.push(array[randomIndex]);
+      usedIndexes.push(randomIndex);
+      i++
+    }
+  }
+  return shuffledAnswers;
+}
+
+//*region Oficial given solution of shuffle function, not used
+
+//dasda hat er nicht als eigene Funktion gespeichert, sonder direkt in dem renderCard (er hat alles auf einmal, ich habe auch noch template)
+//deswegen wird das bei mir möglichst nicht funktionieren
+//außerdem zeigt es alle Antworten zweimal - einmal angegebe Folge, einmal zufällige
+function shuffleAnswerArray() {
+  let answersCopy = [];
+  quizQuestions.answers.forEach(answer => answersCopy.push(answer));
+  while(answersCopy.length > 0) {
+    const randomPointer = Math.floor(Math.random() * answersCopy.length);
+    let answers = answersCopy.splice(randomPointer, 1)[0]; //da uns splice array zurückgibt
+    let answerBtn = document.createElement("button");
+    answerBtn.classList.add("one_answer");
+    answerBtn.innerHTML = oneAnswer.answerContent;
+    answerBtn.id = oneAnswer.answerId;
+    answerBtn.setAttribute("onclick", `validateAnswer(${cardTab.id}, '${oneAnswer.answerId}')`);
+    answers.appendChild(answerBtn);
+  }
+}
+//*endregion
+
+function startTimeTracking() {
+  clearInterval(timeTrackingIntervalId);
+  timerRunning = true;
+  const startTime = Date.now();
+  timeTrackingIntervalId = setInterval(function () {
+    if(timerRunning && quizRunning) {
+      let elapsedTime = Date.now() - startTime;
+      document.getElementById("timerText").textContent = (elapsedTime / 1000).toFixed(1);
+    }
+  }, 100);
+}
+
+function stopTimeTracker() {
+  timerRunning = false;
+  let timeValue = document.getElementById("timer").textContent;
+  responseTimes.push(Number(timeValue));
+}
+
+function renderFinalTab() {
+  const wrapper = document.getElementById("wrapper");
+  wrapper.innerHTML = "";
+  const cardTab = document.createElement("div");
+  cardTab.className = "quiz_tab";
+  const titleContainer = document.createElement("div");
+  titleContainer.classList.add("title_container");
+  const title = document.createElement("h1");
+  title.textContent = "Quiz";
+  const questionTitle = document.createElement("h2");
+  questionTitle.id = "statistikTitle";
+  questionTitle.textContent = "Deine Statistik: ";
+
+  const myData = document.createElement("div");
+  myData.classList.add("one_answer");
+  myData.id = 'myData';
+  const average = document.createElement("p");
+  average.textContent = 'Durchschnittliche Antwortdauer: ' + getAverageTime();
+  const goodOnes = document.createElement("p");
+  goodOnes.textContent = 'Richtig beantwortete Fragen: ' + goodAnswerCounter;
+  const badOnes = document.createElement("p");
+  badOnes.textContent = 'Falsch geklickte Antworten: ' + badAnswerCounter;
+
+  const btnBar = document.createElement("div");
+  btnBar.classList.add("button_bar");
+  const playAgainBtn = document.createElement("button");
+  playAgainBtn.className = "action_button";
+  playAgainBtn.id = 'playAgain';
+  playAgainBtn.textContent = "Nochmal spielen";
+  playAgainBtn.addEventListener('click', () => {renderQuestion()});
+
+  wrapper.appendChild(cardTab);
+  cardTab.appendChild(titleContainer);
+  titleContainer.appendChild(title);
+  cardTab.appendChild(questionTitle);
+  cardTab.appendChild(myData);
+  myData.appendChild(average);
+  myData.appendChild(goodOnes);
+  myData.appendChild(badOnes);
+  cardTab.appendChild(btnBar);
+  btnBar.appendChild(playAgainBtn);
+}
+
+function getAverageTime() {
+  if(responseTimes.length >0) {
+    let timeSum = responseTimes.reduce((sum, num) => {
+    return sum + num;
+  });
+  let averageTime = timeSum / responseTimes.length;
+  return averageTime.toFixed(2);
+  }
+  else {
+    return " Keine Frage beantwortet.";
+  }
+}
+
+function setCountersBack() {
+    questionsCounter = 0;
+    badAnswerCounter = 0;
+    goodAnswerCounter = 0;
+    responseTimes = [];
+}
+
+function createAnswerButtons(givenAnswers, cardId, divElement) {
+    givenAnswers.forEach((oneAnswer) => {
+    const answerBtn = document.createElement("button");
+    answerBtn.classList.add("one_answer");
+    answerBtn.innerHTML = oneAnswer.answerContent;
+    answerBtn.id = oneAnswer.answerId;
+    answerBtn.addEventListener("click", () => {validateAnswer(cardId, oneAnswer.answerId)});
+    divElement.appendChild(answerBtn);
+  });
+}
+
+function getGoodAnswerStyle(idOfReply) {
+    goodAnswerCounter++;
+    document.getElementById(idOfReply).classList.add("correct");
+    jsConfetti.addConfetti({confettiColors: [
+    '#FF8A5B', '#FFD262', '#7ED957', '#75C7FF', '#B679FF', '#FF99C2',
+  ], confettiRadius: 4,});
+}
+
+function getBadAnswerStyle(idOfReply) {
+  badAnswerCounter++;
+  document.getElementById(idOfReply).classList.add("incorrect");
+  document.getElementById(idOfReply).classList.add("shake");
+  document.getElementById(idOfReply).classList.add("incorrect");
+  setTimeout(() => {
+    document.getElementById(idOfReply).classList.remove("incorrect");
+    document.getElementById(idOfReply).classList.remove("shake");
+  }, 850);
+}
---
+++ quizApp2.0/src/questions.js
@@ -0,0 +1,163 @@
+export let quizQuestions = [
+  {
+    questionId: 1,
+    questionName: "Was ist das Besondere an typeof null in JavaScript?",
+    answers: [
+      {
+        answerContent: 'Es ergibt "object".',
+        correct: true,
+        answerId: "a",
+      },
+      {
+        answerContent: 'Es ergibt "null".',
+        correct: false,
+        answerId: "b",
+      },
+      {
+        answerContent: "Es gibt einen SyntaxError.",
+        correct: false,
+        answerId: "c",
+      },
+      {
+        answerContent: 'Es ergibt "undefined".',
+        correct: false,
+        answerId: "d",
+      },
+    ],
+  },
+  {
+    questionId: 2,
+    questionName: "Wann wurde die Programmiersprache JavaScript eingeführt?",
+    answers: [
+      {
+        answerContent: "1964",
+        correct: false,
+        answerId: "a",
+      },
+      {
+        answerContent: "1982",
+        correct: false,
+        answerId: "b",
+      },
+      {
+        answerContent: "1995",
+        correct: true,
+        answerId: "c",
+      },
+      {
+        answerContent: "2002",
+        correct: false,
+        answerId: "d",
+      },
+    ],
+  },
+  {
+    questionId: 3,
+    questionName: "Was bedeuten die Zahlen 1 und 0 in der Informatik?",
+    answers: [
+      {
+        answerContent: "Links/Rechts",
+        correct: false,
+        answerId: "a",
+      },
+      {
+        answerContent: "An/Aus",
+        correct: false,
+        answerId: "b",
+      },
+      {
+        answerContent: "Oben/Unten",
+        correct: false,
+        answerId: "c",
+      },
+      {
+        answerContent: "Wahr/Falsch",
+        correct: true,
+        answerId: "d",
+      },
+    ],
+  },
+  {
+    questionId: 4,
+    questionName:
+      'Welche überraschende Tatsache verbirgt sich hinter dem Ursprung des Begriffs "Computer Bug"?',
+    answers: [
+      {
+        answerContent:
+          "Es war ein tatsächliches Insekt (eine Motte), das einen Rechnerausfall verursachte.",
+        correct: true,
+        answerId: "a",
+      },
+      {
+        answerContent:
+          "Es wurde von einem frühen Programmierer als Witz verwendet.",
+        correct: false,
+        answerId: "b",
+      },
+      {
+        answerContent:
+          "Der Begriff stammt aus der Funktechnik und wurde später übernommen.",
+        correct: false,
+        answerId: "c",
+      },
+      {
+        answerContent:
+          "Er bezieht sich auf einen logischen Fehler in einem Schaltplan.",
+        correct: false,
+        answerId: "d",
+      },
+    ],
+  },
+  {
+    questionId: 5,
+    questionName: "Aus welchem Material war die erste Computermaus, die 1964 von Douglas Engelbart erfunden wurde?",
+    answers: [
+      {
+        answerContent: "Kunststoff",
+        correct: false,
+        answerId: "a",
+      },
+      {
+        answerContent: "Aluminium",
+        correct: false,
+        answerId: "b",
+      },
+      {
+        answerContent: "Holz",
+        correct: true,
+        answerId: "c",
+      },
+      {
+        answerContent: "Gummi",
+        correct: false,
+        answerId: "d",
+      },
+    ],
+  },
+  {
+    questionId: 6,
+    questionName: "Was passiert, wenn du in JavaScript [] + [] in der Konsole eingibst?",
+    answers: [
+      {
+        answerContent: 'Es ergibt undefined, weil Arrays sich nicht addieren lassen.',
+        correct: false,
+        answerId: "a",
+      },
+      {
+        answerContent: 'Ein Fehler, weil Plus zwischen Arrays keinen Sinn ergibt.',
+        correct: false,
+        answerId: "b",
+      },
+      {
+        answerContent: "Ein leerer String – weil JavaScript denkt, du willst concatenaten.",
+        correct: true,
+        answerId: "c",
+      },
+      {
+        answerContent: 'Du öffnest versehentlich ein schwarzes Loch im Callstack.',
+        correct: false,
+        answerId: "d",
+      },
+    ],
+  },
+];
